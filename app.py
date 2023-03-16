3d706c17684e143c5754ec987b976900495d7e07
setuc <setuc@users.noreply.github.com>
2023-03-16 14:44:56 +0800

    Adding a  simple app code
    Adding the code for a chatbot appt

---
+++ app.py
@@ -0,0 +1,52 @@
+import gradio as gr
+
+
+def add_text(history, text):
+    """_summary_
+        The function returns a new history with the text added and an empty string (which is the output).
+    Args:
+        history (_type_): _history is the list of past outputs. It is a list of tuples (text, output).
+        text (_type_): text is the text to be added to the history.
+
+    Returns:
+        _type_: This function returns a new history with the text added (which is the output).
+    """
+    history = history + [(text, None)]
+    return history, ""
+
+
+def add_file(history, file):
+    """_summary_
+
+    Args:
+        history (_type_): history is the list of past outputs. It is a list of tuples (text, output).
+        file (_type_): file is the filename to be added to the history.
+    Returns:
+        _type_: This function returns a new history with the filename added (which is the output).
+    """
+    history = history + [((file.name,), None)]
+    return history
+
+
+def bot(history):
+    response = "**That's cool!**"
+    history[-1][1] = response
+    return history
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+
+    with gr.Row():
+        with gr.Column(scale=0.85):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Enter text and press enter, or upload an image",
+            ).style(container=False)
+        with gr.Column(scale=0.15, min_width=0):
+            btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+
+    txt.submit(add_text, [chatbot, txt], [chatbot, txt]).then(bot, chatbot, chatbot)
+    btn.upload(add_file, [chatbot, btn], [chatbot]).then(bot, chatbot, chatbot)
+
+demo.launch(share=True, inline=True, debug=True, server_port=8080, show_api=False)
